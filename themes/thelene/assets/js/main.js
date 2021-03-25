adb8290cb23051473eaaccb930dc91b89fb52361
Shariful700453 <56506215+Shariful700453@users.noreply.github.com>
2021-03-25 18:02:55 +0600

    footer responsive done

--- themes/thelene/assets/js/main.js
+++ themes/thelene/assets/js/main.js
@@ -271,7 +271,17 @@ $('.wpforms-error').on('click', function(){
       slidesToShow: 1,
       slidesToScroll: 1,
     });
-}
+  }
+
+  if (windowWidth <= 767) {
+    $('.ftr-menu h6').on('click', function(){
+      $(this).toggleClass('active');
+      $(this).parent().siblings().find('h6').removeClass('active');
+      $(this).parent().find('.ftr-menu-des').slideToggle(300);
+      $(this).parent().siblings().find('.ftr-menu-des').slideUp(300);
+    });
+
+  }
 
 
 /*Start of Niaz*/
