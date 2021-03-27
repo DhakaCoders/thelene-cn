2022845b7b72b78377510c000a360b76c0505627
Foridul Islam <foridulsumon@gmail.com>
2021-03-27 16:44:31 +0600

    checkout orders

--- themes/thelene/inc/wc-functions.php
+++ themes/thelene/inc/wc-functions.php
@@ -493,5 +493,13 @@ function bbloomer_display_coupon_form_below_proceed_checkout() {
    <?php
 }
 
+// Remove the payment options form from default location
+remove_action( 'woocommerce_checkout_order_review', 'woocommerce_checkout_payment', 20 );
+
+// Add the payment options form under the "order notes" section
+// Important you will have to also add the following custom CSS to your site:
+// body .woocommerce-checkout-payment { float: none; width: 100%; }
+add_action( 'woocommerce_checkout_after_customer_details', 'woocommerce_checkout_payment', 20 );
+
 include_once(THEME_DIR .'/inc/wc-manage-fields.php');
 
