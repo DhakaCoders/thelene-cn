0050e2a1b486d931a526bd06d9cdcee6aba787e3
azizulsoftsw <azizul.softsw@gmail.com>
2021-04-07 04:30:56 +0600

    Update form-checkout.php

--- themes/thelene/woocommerce/checkout/form-checkout.php
+++ themes/thelene/woocommerce/checkout/form-checkout.php
@@ -23,30 +23,6 @@ $cURL = wc_get_cart_url();
 ?>
 <div class="checkout-page-title">
 	<div class="back-to-dashboard-btn-cntlr"><a class="backshop-cart" href="<?php echo $cURL; ?>">Terug naar winkelmandje</a></div>
-	<h1><?php the_title(); ?></h1>
-</div>
-<?php
-do_action( 'woocommerce_before_checkout_form', $checkout );
-
-// If checkout registration is disabled and not logged in, the user cannot checkout.
-if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_required() && ! is_user_logged_in() ) {
-	echo esc_html( apply_filters( 'woocommerce_checkout_must_be_logged_in_message', __( 'You must be logged in to checkout.', 'woocommerce' ) ) );
-	return;
-}
-
-?>
-
-<form name="checkout" method="post" class="checkout woocommerce-checkout" action="<?php echo esc_url( wc_get_checkout_url() ); ?>" enctype="multipart/form-data">
-
-	<?php 
-	if ( $checkout->get_checkout_fields() ) :
-	?>
-
-		<?php do_action( 'woocommerce_checkout_before_customer_details' ); ?>
-
-		<div class="col2-set" id="customer_details">
-<div class="checkout-page-title">
-	<div class="back-to-dashboard-btn-cntlr"><a class="backshop-cart" href="<?php echo $cURL; ?>">Terug naar winkelmandje</a></div>
 	<div class="checkout-page-bar-crtl">
 		<h1><?php the_title(); ?></h1>
 		<div class="progressbar-crtl">
@@ -80,6 +56,26 @@ if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_requir
 		</div>
 	</div>
 </div>
+<?php
+do_action( 'woocommerce_before_checkout_form', $checkout );
+
+// If checkout registration is disabled and not logged in, the user cannot checkout.
+if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_required() && ! is_user_logged_in() ) {
+	echo esc_html( apply_filters( 'woocommerce_checkout_must_be_logged_in_message', __( 'You must be logged in to checkout.', 'woocommerce' ) ) );
+	return;
+}
+
+?>
+
+<form name="checkout" method="post" class="checkout woocommerce-checkout" action="<?php echo esc_url( wc_get_checkout_url() ); ?>" enctype="multipart/form-data">
+
+	<?php 
+	if ( $checkout->get_checkout_fields() ) :
+	?>
+
+		<?php do_action( 'woocommerce_checkout_before_customer_details' ); ?>
+
+		<div class="col2-set" id="customer_details">
 			<div class="col-1">
 				<?php do_action( 'woocommerce_checkout_billing' ); ?>
 				<?php do_action( 'woocommerce_checkout_shipping' ); ?>
